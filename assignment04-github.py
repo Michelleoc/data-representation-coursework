5f5f383721831a984b5ecd84b6485d8ae7848870
Michelle O'Connor <oconnormichelle1@yahoo.ie>
2022-11-25 12:06:04 +0000

    testing git link still works

--- assignment04-github.py
+++ assignment04-github.py
@@ -9,4 +9,29 @@ import json
 from github import Github
 from config import config as cfg
 
+# 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
+
+url = "https://github.com/Michelleoc/data-representation-coursework"
+url1 = "https://github.com/Michelleoc/aprivateone"
+
 apiKey = cfg["key"]
+
+response = requests.get(url1, auth = ('token', apiKey))
+print (response.status.code)
+
+'''
+g = Github(apiKey)
+
+repo = g.get_repo("Michelleoc/aprivateone")
+
+file_info = repo.get_contents("testing_Andrew_Michelle.txt")
+file_url = file_info.download_url    
+
+response = requests.get(file_url)
+file_content = response.text
+new_file_content = file_content.replace("Andrew", "Michelle")
+
+github_response = repo.update_file(file_info.path,"Replace Andrew with Michelle",new_contents,file_info.sha)
+print (github_response)
+'''
+
---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+    "key" :"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"
+}
+
+# config = {
+#     "key1" : "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"
+# }
